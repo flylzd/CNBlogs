3641dad661f78468c19daf1902720d408ae34ab7
rain-e <314365215@qq.com>
2015-08-17 17:44:03 +0800

    基础框架

--- app/src/main/java/com/lemon/cnblogs/base/BaseActivity.java
+++ app/src/main/java/com/lemon/cnblogs/base/BaseActivity.java
@@ -1,8 +1,121 @@
 package com.lemon.cnblogs.base;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.lemon.cnblogs.AppManager;
+import com.lemon.cnblogs.R;
+import com.orhanobut.logger.Logger;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+import com.umeng.analytics.MobclickAgent;
+
+import butterknife.ButterKnife;
+
 /**
  * 作者：lemon
  * 日期：2015-08-15
  */
-public class BaseActivity  {
+public class BaseActivity extends AppCompatActivity {
+
+    protected LayoutInflater mInflater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Logger.d("onCreate");
+        initSystemBar();
+        AppManager.getAppManager().addActivity(this);
+        if (!hasActionBar()) {
+            supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        }
+        onBeforeSetContentLayout();
+        if (getLayoutId() != 0) {
+            setContentView(getLayoutId());
+        }
+
+        if (hasActionBar()) {
+            initActionBar();
+        }
+        // 通过注解绑定控件
+        ButterKnife.bind(this);
+
+        init(savedInstanceState);
+
+        mInflater = getLayoutInflater();
+    }
+
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        TDevice.hideSoftKeyboard(getCurrentFocus());
+    }
+
+
+    protected void onBeforeSetContentLayout() {
+    }
+
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    protected boolean hasActionBar() {
+        return true;
+    }
+
+    protected View inflateView(int resId) {
+        return mInflater.inflate(resId, null);
+    }
+
+    protected void init(Bundle savedInstanceState) {
+    }
+
+    protected void initActionBar() {
+    }
+
+    private void initSystemBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            setTranslucentStatus(true);
+            // 创建状态栏的管理实例
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            // 激活状态栏设置
+            tintManager.setStatusBarTintEnabled(true);
+            // 激活导航栏设置
+            tintManager.setNavigationBarTintEnabled(true);
+
+            // set a custom tint color for all system bars
+            tintManager.setTintColor(getResources().getColor(R.color.colorPrimary));
+        }
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+
 }
